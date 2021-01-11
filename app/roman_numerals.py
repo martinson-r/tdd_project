6d7538246be5242f02f776b337d38a2fbb61f596
Barry Mattern <bdmattern@gmail.com>
2021-01-11 16:11:28 -0700

    Refactor parse method

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,17 +1,26 @@
+# def parse(numeral):
+#     if numeral == "I":
+#         return 1
+#     elif numeral == "II":
+#         return 2
+#     elif numeral == "III":
+#         return 3
+#     elif numeral == "IV":
+#         return 4
+#     elif numeral == "V":
+#         return 5
+#     elif numeral == "VI":
+#         return 6
+#     elif numeral == "VII":
+#         return 7
+#     elif numeral == "VIII":
+#         return 8
+
+
 def parse(numeral):
-    if numeral == "I":
-        return 1
-    elif numeral == "II":
-        return 2
-    elif numeral == "III":
-        return 3
-    elif numeral == "IV":
-        return 4
-    elif numeral == "V":
-        return 5
-    elif numeral == "VI":
-        return 6
-    elif numeral == "VII":
-        return 7
-    elif numeral == "VIII":
-        return 8
+    roman_numerals = {
+        "I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8
+    }
+    for key in roman_numerals:
+        if numeral == key:
+            return roman_numerals[key]
