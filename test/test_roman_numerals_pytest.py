78ddf540c6ce53854dd3e05f80044435874ae907
Barry Mattern <bdmattern@gmail.com>
2021-01-11 16:28:15 -0700

    Refactor test to parametrize

--- test/test_roman_numerals_pytest.py
+++ test/test_roman_numerals_pytest.py
@@ -1,6 +1,14 @@
 from app.roman_numerals import parse
+from pytest import mark
 
 
-def test_roman_numeral_parser():
-    result = parse("IX")
-    assert result == 9
+# def test_roman_numeral_parser():
+#     result = parse("IX")
+#     assert result == 9
+
+
+@mark.parametrize("s,expected", [("IX", 9), ("X", 10)])
+def test_roman_numeral_parser(s, expected):
+    result = parse(s)
+
+    assert result == expected
