92247e05890b48270d9000659eeb118facd5dda8
Barry Mattern <bdmattern@gmail.com>
2021-01-11 16:18:20 -0700

    Solve for IX test

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -19,7 +19,15 @@
 
 def parse(numeral):
     roman_numerals = {
-        "I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8
+        "I": 1,
+        "II": 2,
+        "III": 3,
+        "IV": 4,
+        "V": 5,
+        "VI": 6,
+        "VII": 7,
+        "VIII": 8,
+        "IX": 9
     }
     for key in roman_numerals:
         if numeral == key:
